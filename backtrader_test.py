bcfb941067277528fde96812f6f79b9b4a616e25
Henrikki Helander <henrikki.helander@gmail.com>
2025-10-23 21:19:21 +0300

    tuplaloopitus

--- backtrader_test.py
+++ backtrader_test.py
@@ -11,28 +11,28 @@ if __name__ == '__main__':
     data_loader = GetData('BTC-USD', '2022-01-01', '1d')
     df = data_loader.load()
     aloitus_rahat = 10000
-    maperiods = [10, 20, 50, 100, 150, 200]
+    pslows = [50, 100, 150, 200, 250]
+    psfasts = [2, 5, 10, 15, 20]
     sizer = 98
     commission = 0.02
     result_list = []
 
-    for period in maperiods:             
-        data = bt.feeds.PandasData(dataname=df)
-        cerebro = bt.Cerebro()
-        cerebro.adddata(data)
+    for psfast in psfasts:
+        for pslow in pslows:             
+            data = bt.feeds.PandasData(dataname=df)
+            cerebro = bt.Cerebro()
+            cerebro.adddata(data)
 
-        # Prosentti on prosentti käteisestä joka sijoitetaan. Jos 95% ja komissio 6% niin 101% eli ei ole rahaa ostaa.
-        cerebro.addsizer(bt.sizers.PercentSizer, percents=sizer) 
-        cerebro.broker.set_cash(aloitus_rahat)
-        cerebro.broker.setcommission(commission=commission)
-        # Komissio + prosentti = kokonaiskulu. Jos menee yli 100% niin ei ole rahaa ostaa.
+            # Prosentti on prosentti käteisestä joka sijoitetaan. Jos 95% ja komissio 6% niin 101% eli ei ole rahaa ostaa.
+            cerebro.addsizer(bt.sizers.PercentSizer, percents=sizer) 
+            cerebro.broker.set_cash(aloitus_rahat)
+            cerebro.broker.setcommission(commission=commission)
+            cerebro.addstrategy(SMACross, pfast=psfast, pslow=pslow, printlog=False)
+            cerebro.run()
 
-        cerebro.addstrategy(SMACross, pfast=5, pslow=period, printlog=False)
-        cerebro.run()
+            voitto_ratio = ((cerebro.broker.getvalue() / aloitus_rahat) - 1) * 100
+            result_list.append((pslow, psfast, voitto_ratio, cerebro.broker.getvalue(),cerebro.runstrats[0][0].sellcount, cerebro.runstrats[0][0].buycount))
+            #cerebro.plot()
 
-        voitto_ratio = ((cerebro.broker.getvalue() / aloitus_rahat) - 1) * 100
-        result_list.append((period, voitto_ratio, cerebro.broker.getvalue(),cerebro.runstrats[0][0].sellcount, cerebro.runstrats[0][0].buycount))
-        #cerebro.plot()
-
-    par_df = pd.DataFrame(result_list, columns = ['maperiod', 'return', 'rahaa','sellcount', 'buycount'])
+    par_df = pd.DataFrame(result_list, columns = ['pslow', 'psfast', 'return', 'rahaa','sellcount', 'buycount'])
     print(par_df)
